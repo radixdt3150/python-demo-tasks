a1b7c34cd95d0ec6eca4326fdcef2468e9663c45
radixdt3150 <mayur.upadhyay@radixweb.com>
2024-08-30 11:36:52 +0530

    chore: Adding basic scripts

---
+++ general/car.py
@@ -0,0 +1,10 @@
+# This is an example to demonstrate module in python
+def drive():
+    print("Car is driving")
+
+def steer():
+    print("We are now steering")
+
+if __name__ == "__main__":
+    import sys
+    print(sys.argv[2])
